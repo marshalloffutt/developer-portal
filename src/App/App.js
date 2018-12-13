ddb68443f9fff1b649a3eb93298f3d5de5669f4e
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-12 19:52:55 -0600

    state stuff and divs

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 
 import connection from '../helpers/data/connection';
+import Tutorials from '../components/Tutorials/Tutorials';
+import Profile from '../components/Profile/Profile';
+import Commits from '../components/Commits/Commits';
 
 import Auth from '../components/Auth/Auth';
 
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
     return (
-      <div className="Auth">
-        <Auth />
+      <div className="App">
+        <Tutorials />
+        <Profile />
+        <Commits />
       </div>
     );
   }
---
+++ src/components/Commits/Commits.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Commits.scss';
+
+class Commits extends React.Component {
+  render() {
+    return (
+      <div className="commits">
+        <h2>Commits</h2>
+      </div>
+    );
+  }
+}
+
+export default Commits;
