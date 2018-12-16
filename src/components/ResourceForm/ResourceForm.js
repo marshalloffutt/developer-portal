1be41c33e05af88c00b89b2ae2e0a20fb5a6a1ab
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-16 09:38:35 -0600

    adds data for resources

---
+++ src/components/ResourceForm/ResourceForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ResourceForm.scss';
+
+class ResourceForm extends React.Component {
+  render() {
+    return (
+      <div className="resourceForm col">
+        <h2>Resource Form</h2>
+      </div>
+    );
+  }
+}
+
+export default ResourceForm;
---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Resources.scss';
+
+class Resources extends React.Component {
+  render() {
+    return (
+      <div className="resources col">
+        <h2>Resources</h2>
+      </div>
+    );
+  }
+}
+
+export default Resources;
