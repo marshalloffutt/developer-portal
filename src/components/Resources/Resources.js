9bb7303835f1b2ab27cd1ee672549f22d17c3b2c
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-16 10:50:02 -0600

    Adds resourceShape to be used later

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import resourceShape from '../../helpers/propz/resourceShape';
 import './Resources.scss';
 
 class Resources extends React.Component {
+  static propTypes = {
+    resources: PropTypes.arrayOf(resourceShape),
+  }
+
   render() {
     return (
       <div className="resources col">
