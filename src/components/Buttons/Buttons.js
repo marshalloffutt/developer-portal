a985ff951b110278aa4f382bcef390bdaf87297e
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-17 21:31:52 -0600

    filter event in progress

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button, ButtonGroup } from 'reactstrap';
 
 class FilterButtons extends React.Component {
+  static propTypes = {
+    filterAll: PropTypes.func,
+    filterTutorials: PropTypes.func,
+    filterBlogs: PropTypes.func,
+    filterPodcasts: PropTypes.func,
+    filterDocs: PropTypes.func,
+  }
+
+  showAll = (e) => {
+    e.preventDefault();
+    const { filterAll } = this.props;
+    filterAll();
+  }
+
+  showTutorials = (e) => {
+    e.preventDefault();
+    const { filterTutorials } = this.props;
+    filterTutorials();
+  }
+
+  showBlogs = (e) => {
+    e.preventDefault();
+    const { filterBlogs } = this.props;
+    filterBlogs();
+  }
+
+  showPodcasts = (e) => {
+    e.preventDefault();
+    const { filterPodcasts } = this.props;
+    filterPodcasts();
+  }
+
+  showDocs = (e) => {
+    e.preventDefault();
+    const { filterDocs } = this.props;
+    filterDocs();
+  }
+
   render() {
     return (
       <ButtonGroup>
@@ -9,7 +48,7 @@ class FilterButtons extends React.Component {
         <Button className="btn btn-primary" onClick={this.showTutorials}>Tutorials</Button>
         <Button className="btn btn-primary" onClick={this.showBlogs}>Blogs</Button>
         <Button className="btn btn-primary" onClick={this.showPodcasts}>Podcasts</Button>
-        <Button className="btn btn-primary" onClick={this.showDocs}>Documentation</Button>
+        <Button className="btn btn-primary" onClick={this.showDocs}>Docs</Button>
       </ButtonGroup>
     );
   }
